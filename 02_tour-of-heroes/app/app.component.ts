2adad77ef61bee504f049d5e34885ad2a524ea53
Sebastien Jacques <jac.sebastien@gmail.com>
2016-11-04 16:37:35 +0100

    template, styles and objects

--- 02_tour-of-heroes/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: '<h1>Hello world !</h1><h2>This is my first Angular 2 application.</h2>'
-})
-export class AppComponent { }
