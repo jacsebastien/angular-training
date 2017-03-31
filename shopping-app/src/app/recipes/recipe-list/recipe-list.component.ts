699f4d01ef2088b6b64e065b3ac230726e881297
jacsebastien <jac.sebastien@gmail.com>
2017-03-31 15:12:57 +0200

    bind data of the recipe

--- shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  // create an array type of Recipe and create new Recipe inside it
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/899px-Recipe-575434.svg.png'),
+    new Recipe('A Second Recipe', 'This is an other test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/899px-Recipe-575434.svg.png')
+  ];
 
   constructor() { }
 
