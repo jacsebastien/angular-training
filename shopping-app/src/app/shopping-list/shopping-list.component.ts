fed692a329bbcbb9cff1c26eea40caa58c27978a
jacsebastien <jac.sebastien@gmail.com>
2017-03-31 15:41:46 +0200

    add ingredients from model + binding in view

--- shopping-app/src/app/shopping-list/shopping-list.component.ts
+++ shopping-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
-  
+  ingredients: Ingredient[] = [
+    new Ingredient('Kiwi', 2),
+    new Ingredient('Appel', 4)
+  ];
+
   constructor() { }
 
   ngOnInit() {
