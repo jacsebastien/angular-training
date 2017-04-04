27cdb9a8fc1501b4292b625c9db3c4bf6f6034b8
jacsebastien <jac.sebastien@gmail.com>
2017-04-04 15:50:27 +0200

    split into multiple components

---
+++ cmp-databinding-start/src/app/cokpit/cokpit.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cokpit',
+  templateUrl: './cokpit.component.html',
+  styleUrls: ['./cokpit.component.css']
+})
+export class CokpitComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+}
