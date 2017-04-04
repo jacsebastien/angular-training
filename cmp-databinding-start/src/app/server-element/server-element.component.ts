b0d40340ed744e97c2e092972be125db32c2933c
jacsebastien <jac.sebastien@gmail.com>
2017-04-04 17:21:03 +0200

    view encapsulation

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None, Native 
+  // None disable the default Emulated encapsulation that create DOM tags propeties to know which view come from whic component and apply css only on it
+  // Native use the DOM shadow not supported by all browsers
 })
 export class ServerElementComponent implements OnInit {
   // define property type of object without any value
