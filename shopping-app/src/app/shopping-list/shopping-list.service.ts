8510571c51c836e23cd69c4c6a010bc041815863
jacsebastien <jac.sebastien@gmail.com>
2017-05-31 16:41:52 +0200

    use service to add ingredients to array

--- shopping-app/src/app/shopping-list/shopping-list.service.ts
+++ shopping-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,16 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class ShoppingListService {
-    
+    private ingredients: Ingredient[] = [
+        new Ingredient('Kiwi', 2),
+        new Ingredient('Appel', 4)
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+    }
 }
