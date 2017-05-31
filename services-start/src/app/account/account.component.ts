f5baa33ed6c56c5ef81115aa8b6f989acdec6728
jacsebastien <jac.sebastien@gmail.com>
2017-05-31 14:37:42 +0200

    just clean code

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -1,23 +1,19 @@
 import { Component, Input } from '@angular/core';
-import { LoggingService } from '../logging.service';
 import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  // providers: [LoggingService]
+  styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
 
   // AccountService is already instancied in providers of AppModule so we just need to add it in constructor and not in providers of this component to get the same instance here
-  constructor(private loggingService: LoggingService,
-              private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   onSetTo(status: string) {
     this.accountService.updateAccount(this.id, status);
-    // this.loggingService.logStatusChange(status);
   }
 }
