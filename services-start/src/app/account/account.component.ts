c025fa6ebe99dab1809f819b7fe1c1336c3e3c2a
jacsebastien <jac.sebastien@gmail.com>
2017-05-31 14:26:21 +0200

    same instance of the service for all components

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -6,12 +6,13 @@ import { AccountService } from '../account.service';
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService, AccountService]
+  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
 
+  // AccountService is already instancied in providers of AppComponent so we just need to add it in constructor and not in providers of this component to get the same instance here
   constructor(private loggingService: LoggingService,
               private accountService: AccountService) {}
 
