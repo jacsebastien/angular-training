b9825664d3a7d5cd0dce11e9e0eefa2de2340766
jacsebastien <jac.sebastien@gmail.com>
2017-06-12 15:10:11 +0200

    offline project based on routing project folder

---
+++ theory/routing-start-offline/sw-precache-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  navigateFallback: '/index.html',
+  stripPrefix: 'dist',
+  root: 'dist/',
+  staticFileGlobs: [
+    'dist/**.html',
+    'dist/**.js',
+    'dist/**.css',
+    'dist/assets/images/*',
+    'dist/assets/icons/*'
+  ],
+  runtimeCaching: [{
+    urlPattern: /my-custom\.api-url\.com/,
+    handler: 'networkFirst'
+  }]
+};
