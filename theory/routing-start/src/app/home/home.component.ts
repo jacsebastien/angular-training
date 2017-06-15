51ccc4260fda026d1391210446428d4b39df1d1c
jacsebastien <jac.sebastien@gmail.com>
2017-06-15 16:17:13 +0200

    pass query params with links and programatically

--- theory/routing-start/src/app/home/home.component.ts
+++ theory/routing-start/src/app/home/home.component.ts
@@ -2,23 +2,34 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router:Router) { }
+    constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  onLoadServers() {
-    
-    // complex calculation
+    onLoadServers() {
+        // complex calculation
 
-    // array with the different parts of the url tha we will navigate on
-    this.router.navigate(['/servers']);
-  }
+        // array with the different parts of the url tha we will navigate on
+        this.router.navigate(['/servers']);
+    }
+
+    onLoadServer(id: number) {
+        // complex calculation
+
+        this.router.navigate(
+            ['/servers', id, 'edit'], 
+            { 
+                queryParams: { allowEdit: '1' },
+                fragment: 'loading'
+            }
+        );
+    }
 
 }
