20763bdf262fc2575714195cbec062882f9d4a65
jacsebastien <jac.sebastien@gmail.com>
2017-06-15 16:26:56 +0200

    get data from route parameters

--- theory/routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ theory/routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,27 +1,37 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
 
 @Component({
-  selector: 'app-edit-server',
-  templateUrl: './edit-server.component.html',
-  styleUrls: ['./edit-server.component.css']
+    selector: 'app-edit-server',
+    templateUrl: './edit-server.component.html',
+    styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
-  serverName = '';
-  serverStatus = '';
+    server: { id: number, name: string, status: string };
+    serverName = '';
+    serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+    constructor(private serversService: ServersService,
+                private route: ActivatedRoute) { }
 
-  ngOnInit() {
-    this.server = this.serversService.getServer(1);
-    this.serverName = this.server.name;
-    this.serverStatus = this.server.status;
-  }
+    ngOnInit() {
+        // get dirct access to data sent in the url on component creation
+        console.log(this.route.snapshot.queryParams);
+        console.log(this.route.snapshot.fragment);
 
-  onUpdateServer() {
-    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
-  }
+        // subscribe on route parameters changes to get updated one and processed it
+        this.route.queryParams.subscribe();
+        this.route.fragment.subscribe();
+
+        this.server = this.serversService.getServer(1);
+        this.serverName = this.server.name;
+        this.serverStatus = this.server.status;
+    }
+
+    onUpdateServer() {
+        this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
+    }
 
 }
