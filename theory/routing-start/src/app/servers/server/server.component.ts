f52315f723cdead9278a503a1ddb4e4c9c9dc58d
jacsebastien <jac.sebastien@gmail.com>
2017-06-15 16:47:02 +0200

    get dynamic data from route

--- theory/routing-start/src/app/servers/server/server.component.ts
+++ theory/routing-start/src/app/servers/server/server.component.ts
@@ -1,19 +1,32 @@
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
 
 @Component({
-  selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+    server: { id: number, name: string, status: string };
 
-  constructor(private serversService: ServersService) { }
+    constructor(private serversService: ServersService,
+                private route: ActivatedRoute) { }
 
-  ngOnInit() {
-    this.server = this.serversService.getServer(1);
-  }
+    ngOnInit() {
+        // user "+" operator to convert the id passed like a string to a number
+        const serverId = +this.route.snapshot.params['id'];
+        this.server = this.serversService.getServer(serverId);
+
+        // get new data whenever params changed
+        this.route.params
+            .subscribe(
+                (params: Params) => {
+                    // get new server with string id converted to number
+                    this.server = this.serversService.getServer(+params['id']);
+                }
+            );
+    }
 
 }
