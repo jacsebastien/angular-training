3c7d87d9afb66a65d0700a18bcee88a0feecca0d
Sebastien Jacques <jac.sebastien@gmail.com>
2017-06-16 19:05:14 +0200

    add authguard to routes and app module

--- theory/routing-start/src/app/app-routing.module.ts
+++ theory/routing-start/src/app/app-routing.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthGuard } from './auth-guard.service';
 import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
+import { ServersComponent } from './servers/servers.component';
 import { ServersService } from './servers/servers.service';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UserComponent } from './users/user/user.component';
+import { UsersComponent } from './users/users.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'users', component: UsersComponent, children: [
         { path: ':id/:name', component: UserComponent },
     ] },
-    { path: 'servers', component: ServersComponent, children: [
+    // use AuthGuard service to manage access to servers route and all his children
+    { path: 'servers', canActivate: [AuthGuard] , component: ServersComponent, children: [
         // nested route /servers/:id
         { path: ':id', component: ServerComponent },
         { path: ':id/edit', component: EditServerComponent }
--- theory/routing-start/src/app/app.module.ts
+++ theory/routing-start/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
+import { ServersComponent } from './servers/servers.component';
 import { ServersService } from './servers/servers.service';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModule } from './app-routing.module';
+import { UserComponent } from './users/user/user.component';
 
 @NgModule({
     declarations: [
@@ -31,7 +33,7 @@ import { AppRoutingModule } from './app-routing.module';
         HttpModule,
         AppRoutingModule
     ],
-    providers: [ServersService],
+    providers: [ServersService, AuthService, AuthGuard],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
