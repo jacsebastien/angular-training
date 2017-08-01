9f739a4d124719ea4c7b0fb0f6c3698223fb9220
jacsebastien <jac.sebastien@gmail.com>
2017-08-01 16:10:57 +0200

    use observable "interval" emitter

--- theory/observables-start/src/app/home/home.component.ts
+++ theory/observables-start/src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';   // needed to work with Observable operators like "interval"
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // create an observable which emit numbers each seconds
+        const myNumbers = Observable.interval(1000);
+
+        myNumbers.subscribe(
+            (number: number) => {
+                console.log(number);
+            }
+        );
+    }
 
 }
