0baf8d257d955be517eba41a83cb5119ea362a66
jacsebastien <jac.sebastien@gmail.com>
2017-08-01 16:24:47 +0200

    create custom observable

--- theory/observables-start/src/app/home/home.component.ts
+++ theory/observables-start/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';   // needed to work with Observable operators like "interval"
+import { Observer } from "rxjs/Observer";
 
 @Component({
     selector: 'app-home',
@@ -12,12 +13,41 @@ export class HomeComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-        // create an observable which emit numbers each seconds
-        const myNumbers = Observable.interval(1000);
+        // // create an observable which emit numbers each seconds
+        // const myNumbers = Observable.interval(1000);
 
-        myNumbers.subscribe(
-            (number: number) => {
-                console.log(number);
+        // myNumbers.subscribe(
+        //     (number: number) => {
+        //         console.log(number);
+        //     }
+        // );
+
+        // build a bridge between observable and observer to know whenever it's fired
+        // observer return a string
+        const myObservable = Observable.create((observer: Observer<string>) => {
+            setTimeout(() => {
+                // "next" juste pass to the next data package
+                observer.next('first package');
+            }, 2000);
+            setTimeout(() => {
+                observer.next('second package');
+            }, 4000);
+            setTimeout(() => {
+                // create a fail
+                // observer.error('this does not work!');
+
+                // complete observer
+                observer.complete();
+            }, 5000);
+        });
+
+        myObservable.subscribe(
+            (data: string) => {
+                console.log(data);
+            }, (error: string) => {
+                console.log(error);
+            }, () => {
+                console.log("Completed");
             }
         );
     }
