d70222f0ac91f6ff174a9b46f41e0369a6db7778
jacsebastien <jac.sebastien@gmail.com>
2017-08-04 16:08:39 +0200

    submit form and get access to data

--- theory/forms-td-start/src/app/app.component.ts
+++ theory/forms-td-start/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
+    suggestUserName() {
+        const suggestedName = 'Superuser';
+    }
+
+    // triggered when the form is submitted and get reference to it for arguments
+    onSubmit(form: NgForm) {
+        console.log(form);
+    }
 }
