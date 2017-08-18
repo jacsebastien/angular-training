50be6c2160411b0730882bb93a484647225a7c45
jacsebastien <jac.sebastien@gmail.com>
2017-08-18 12:28:42 +0200

    bind form code with template add validators show messages if invalid style for invalid input

--- theory/forms-reactive-start/src/app/app.component.ts
+++ theory/forms-reactive-start/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit {
+    genders = ['male', 'female'];
+
+    // create an angular form
+    signupForm: FormGroup;
+
+    ngOnInit() {
+        // init form and create form controls with some default values and validators
+        this.signupForm = new FormGroup({
+            'username': new FormControl(null, Validators.required),
+            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'gender': new FormControl('male')
+        });
+    }
+
+    onSubmit() {
+        console.log(this.signupForm);
+    }
 }
