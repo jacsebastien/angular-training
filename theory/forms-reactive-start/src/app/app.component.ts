188facabf1f1e4a3ffbd626f686da06a6aebaea7
jacsebastien <jac.sebastien@gmail.com>
2017-08-18 13:59:54 +0200

    use for groups inside form

--- theory/forms-reactive-start/src/app/app.component.ts
+++ theory/forms-reactive-start/src/app/app.component.ts
@@ -15,8 +15,10 @@ export class AppComponent implements OnInit {
     ngOnInit() {
         // init form and create form controls with some default values and validators
         this.signupForm = new FormGroup({
-            'username': new FormControl(null, Validators.required),
-            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'userData': new FormGroup({
+                'username': new FormControl(null, Validators.required),
+                'email': new FormControl(null, [Validators.required, Validators.email])
+            }),
             'gender': new FormControl('male')
         });
     }
