8b95ce1cd1ab2fed85750ef74aeb2a5b3bf864fe
jacsebastien <jac.sebastien@gmail.com>
2017-08-28 15:12:39 +0200

    use parameters

--- theory/pipes-start/src/app/shorten.pipe.ts
+++ theory/pipes-start/src/app/shorten.pipe.ts
@@ -4,10 +4,10 @@ import { PipeTransform, Pipe } from "@angular/core";
     name: 'shorten'
 })
 export class ShortenPipe implements PipeTransform {
-    transform(value: any) {
-        if(value.length > 10) {
-            // return value with only 10 firsts characters
-            return value.substr(0, 10) + " ...";
+    transform(value: any, limit: number) {
+        if(value.length > limit) {
+            // return value with only x firsts characters
+            return value.substr(0, limit) + " ...";
         }
 
         return value;
