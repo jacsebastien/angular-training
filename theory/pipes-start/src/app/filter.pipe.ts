701d4eb674bd400ef0da8916a08546d9dd1c800d
jacsebastien <jac.sebastien@gmail.com>
2017-08-28 16:00:27 +0200

    add filter pipe

---
+++ theory/pipes-start/src/app/filter.pipe.ts
@@ -0,0 +1,30 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+    /*  
+        value: array of objects to filter
+        filterString: string that will filter array
+        propName: property that will be focusend for filtering inside array 
+    */
+    transform(value: any, filterString: string, propName: string): any {
+        let resultArray = [];
+        
+        // if list is empty or user entered nothing
+        if(value.length === 0 || filterString === '') {
+            return value;
+        }        
+        
+        for(const item of value) {
+            if(item[propName] === filterString) {
+                resultArray.push(item);
+            }
+        }
+
+        return resultArray.length > 0 ? resultArray : value;
+    }
+
+}
