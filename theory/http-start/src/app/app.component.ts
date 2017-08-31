3baf2487441afbc7f0f03807416ac3e77847d226
jacsebastien <jac.sebastien@gmail.com>
2017-08-31 14:49:28 +0200

    get data put data transform recieved data use async in template

--- theory/http-start/src/app/app.component.ts
+++ theory/http-start/src/app/app.component.ts
@@ -3,55 +3,58 @@ import { Component } from '@angular/core';
 import { Response } from '@angular/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers = [
-    {
-      name: 'Testserver',
-      capacity: 10,
-      id: this.generateId()
-    },
-    {
-      name: 'Liveserver',
-      capacity: 100,
-      id: this.generateId()
-    }
-  ];
-
-  constructor(private serverService: ServerService) {}
-
-  onAddServer(name: string) {
-    this.servers.push({
-      name: name,
-      capacity: 50,
-      id: this.generateId()
-    });
-  }
-
-  onSave() {
-      this.serverService.storeServers(this.servers)
-      .subscribe(
-        response => console.log(response),
-        reason => console.log(reason)
-      );
-  }
-
-  onGet() {
-      this.serverService.getServers()
-      .subscribe(
-        (response: Response) => {
-            // .json get content of body and turn it to a javascript object
-            const data = response.json();
-            console.log(data)
+    appName = this.serverService.getAppName();
+    servers = [
+        {
+            name: 'Testserver',
+            capacity: 10,
+            id: this.generateId()
         },
-        reason => console.log(reason)
-      );
-  }
+        {
+            name: 'Liveserver',
+            capacity: 100,
+            id: this.generateId()
+        }
+    ];
 
-  private generateId() {
-    return Math.round(Math.random() * 10000);
-  }
+    constructor(private serverService: ServerService) { 
+        // this.appName = this.serverService.getAppName();
+    }
+
+    onAddServer(name: string) {
+        this.servers.push({
+            name: name,
+            capacity: 50,
+            id: this.generateId()
+        });
+    }
+
+    onSave() {
+        this.serverService.storeServers(this.servers)
+            .subscribe(
+            response => console.log(response),
+            reason => console.log(reason)
+            );
+    }
+
+    onGet() {
+        this.serverService.getServers()
+            .subscribe(
+            // servers is an array
+            (servers: any[]) => {
+                console.log(servers);
+                this.servers = servers;
+            },
+            reason => console.log(reason)
+            );
+    }
+
+    private generateId() {
+        return Math.round(Math.random() * 10000);
+    }
 }
--- theory/http-start/src/app/server.service.ts
+++ theory/http-start/src/app/server.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from "@angular/http";
+import { Http, Headers, Response } from "@angular/http";
 
 import { Observable } from 'rxjs/Observable';
+// allow use of observable operators
+import 'rxjs/RX';
 
 @Injectable()
 export class ServerService {
@@ -14,16 +16,42 @@ export class ServerService {
         });
 
         // create an observable which will send data ONLY if a component subscribes to it
-        return this.http.post('https://udemy-ng-http-4bbe4.firebaseio.com/data.json', 
+        // return this.http.post('https://udemy-ng-http-4bbe4.firebaseio.com/data.json', 
+        //     servers, 
+        //     { headers: headers }
+        // );
+        return this.http.put('https://udemy-ng-http-4bbe4.firebaseio.com/data.json', 
             servers, 
-            {
-                headers: headers
-            }
+            { headers: headers }
         );
     }
 
     getServers(): Observable<any> {
-        return this.http.get('https://udemy-ng-http-4bbe4.firebaseio.com/data.json');
+        // map() take data from the .get(), allow to transforme it and return it, wrapped into another observable
+        return this.http.get('https://udemy-ng-http-4bbe4.firebaseio.com/data.json')
+            .map((response: Response) => {
+                const data = response.json();
+                for(let server of data) {
+                    server.name = 'FETCHED_' + server.name;
+                }
+                // return simple object which will be wrapped into a new observable
+                return data;
+            })
+            .catch(
+                (error: Response) => {
+                    // console.log(error);
+
+                    // create an observable to return the error
+                    return Observable.throw('Something went wrong');
+                }
+            );
+    }
+
+    getAppName() {
+        return this.http.get('https://udemy-ng-http-4bbe4.firebaseio.com/appName.json')
+        .map((response: Response) => {
+            return response.json();
+        });
     }
 
 }
