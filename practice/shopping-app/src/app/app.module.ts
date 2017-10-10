b530c02d3685b2aa8a24baf1ea485a726d1d5cea
jacsebastien <jac.sebastien@gmail.com>
2017-10-10 15:25:00 +0200

    indent

--- practice/shopping-app/src/app/app.module.ts
+++ practice/shopping-app/src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { AuthModule } from './auth/auth.module';
-import { BrowserModule }    from '@angular/platform-browser';
-import { NgModule }         from '@angular/core';
-import { HttpModule }       from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
-import { AppComponent }             from './app.component';
-import { HeaderComponent }          from './header/header.component';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
-import { ShoppingListService }  from './shopping-list/shopping-list.service';
-import { RecipeService }        from './recipes/recipe.service';
-import { DataStorageService }   from './shared/data-storage.service';
-import { AuthService }          from './auth/auth.service';
-import { AuthGuard }            from './auth/auth-guard.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipes/recipe.service';
+import { DataStorageService } from './shared/data-storage.service';
+import { AuthService } from './auth/auth.service';
+import { AuthGuard } from './auth/auth-guard.service';
 import { HomeComponent } from './home/home.component';
 
 // Use RecipesModule to wrap all recipes features
