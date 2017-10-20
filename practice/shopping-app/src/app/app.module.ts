9aad3b4c6e83f35c6ab627c9a5db9bd200891d32
jacsebastien <jac.sebastien@gmail.com>
2017-10-20 16:12:44 +0200

    create core module

--- practice/shopping-app/src/app/app.module.ts
+++ practice/shopping-app/src/app/app.module.ts
@@ -4,28 +4,25 @@ import { HttpModule } from '@angular/http';
 
 import { SharedModule } from './shared/shared.module';
 
+import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
-import { HomeComponent } from './home/home.component';
 
 // Use RecipesModule to wrap all recipes features
 // import it in app module to avoid import of a lot of components and clean up app module
 // use custom sharedModule to access some directives needed in app module and featured modules like recipes
 @NgModule({
     declarations: [
-        AppComponent,
-        HeaderComponent,
-        HomeComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
@@ -33,7 +30,8 @@ import { HomeComponent } from './home/home.component';
         AppRoutingModule,
         SharedModule,
         ShoppingListModule,
-        AuthModule
+        AuthModule,
+        CoreModule
     ],
     providers: [
         ShoppingListService,
