820606af6582977c741a39d1bc8aa28f134bf281
jacsebastien <jac.sebastien@gmail.com>
2018-01-10 10:17:08 +0100

    add comments

--- practice/shopping-app/src/app/shopping-list/store/shopping-list.actions.ts
+++ practice/shopping-app/src/app/shopping-list/store/shopping-list.actions.ts
@@ -2,11 +2,14 @@ import { Action } from '@ngrx/store';
 
 import { Ingredient } from '../../shared/ingredient.model';
 
+// add a constant used to know what the action is doing
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
 export class AddIngredient implements Action {
-    readonly type = ADD_INGREDIENT;
+    readonly type = ADD_INGREDIENT;  // need to be provided to correspond to Action interface
+    // add the payload that we need to manage data
     payload: Ingredient;
 }
 
+// export ShoppingListActions which will contains all of our exported actions
 export type ShoppingListActions = AddIngredient;
